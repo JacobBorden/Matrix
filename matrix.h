147f34bebb22cb4c0d58257da587dd387d9d8683
Jacob Borden <amenra.beats@gmail.com>
2022-07-15 09:26:21 -0300

    Wrote Row Iterator Code

--- matrix.h
+++ matrix.h
@@ -2,6 +2,69 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+#include <algorithm>
+#include <time.h>
+
+template <typename MatrixRow>
+class MatrixRowIterator
+{
+    public:
+    using value_type = typename MatrixRow::value_type;
+    using pointer = value_type*;
+    using reference = value_type&;
+
+    MatrixRowIterator(pointer ptr)::m_ptr(ptr){}
+
+    MatrixRowIterator& operator++()
+    {
+        m_ptr++;
+        return *this;
+    }
+
+MatrixRowIterator operator++(int)
+{
+    MatrixRowIterator it = *this;
+    ++(this);
+    return it;
+}
+
+MatrixRowIterator& operator--()
+{
+    m_ptr--;
+    return *this;
+}
+
+MatrixRowIterator operator--(int)
+{
+    MatrixRowIterator it = *this;
+    --(this);
+    return it;
+}
+
+pointer operator->()
+{
+    return m_ptr;
+}
+
+reference operator*()
+{
+    return *m_ptr;
+}
+
+bool operator==(MatrixRowIterator other)
+{
+    return this->m_ptr == other.m_ptr;
+}
+
+bool operator!=(MatrixRowIterator other)
+{
+    return this->m_ptr != other.m_ptr;
+}
+
+private:
+pointer m_ptr;
+
+};
 
 
 #endif 
